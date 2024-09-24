4d1cada8e024e400e5e3d7b823e7a375ea4df769
Xenos <acristyano22@gmail.com>
2024-09-24 19:18:11 +0100

    init prisma and resolvers

--- main.ts
+++ main.ts
@@ -1,13 +1,63 @@
 import { Elysia } from "elysia";
 import { yoga } from "@elysiajs/graphql-yoga";
 import { useCookies } from "@whatwg-node/server-plugin-cookies";
+import prisma from "./utils/primsa"; // Corrected the import name
 
 const app = new Elysia()
   .use(
     yoga({
       typeDefs: /* GraphQL */ `
+        enum Role {
+          USER
+          CHEF
+          ADMIN
+        }
+        scalar DateTime
+
+        type User {
+          id: ID!
+          first_name: String
+          last_name: String
+          email: String
+          role: Role
+          createdAt: DateTime
+          updatedAt: DateTime
+          posts: [Post]
+          department: Department
+        }
+
+        type Post {
+          id: ID!
+          content: String
+          author: User
+          image: String
+          createdAt: DateTime
+          updatedAt: DateTime
+        }
+
+        type Department {
+          id: ID!
+          name: String
+          chef: User
+          TVs: [TV]
+        }
+
+        type TV {
+          id: ID!
+          name: String
+          department: Department
+          password: String
+        }
+
         type Query {
-          hi: String
+          users: [User]
+          user(id: ID!): User
+          posts: [Post]
+          post(id: ID!): Post
+          departments: [Department]
+          department(id: ID!): Department
+          TVs: [TV]
+          TV(id: ID!): TV
         }
       `,
       context: {
@@ -16,7 +66,46 @@ const app = new Elysia()
       useContext(_) {},
       resolvers: {
         Query: {
-          hi: async (parent, args, context) => context.name,
+          users() {
+            return prisma.user.findMany();
+          },
+          user(_, args) {
+            return prisma.user.findUnique({
+              where: {
+                id: args.id,
+              },
+            });
+          },
+          posts() {
+            return prisma.post.findMany();
+          },
+          post(_, args) {
+            return prisma.post.findUnique({
+              where: {
+                id: args.id,
+              },
+            });
+          },
+          departments() {
+            return prisma.department.findMany();
+          },
+          department(_, args) {
+            return prisma.department.findUnique({
+              where: {
+                id: args.id,
+              },
+            });
+          },
+          TVs() {
+            return prisma.tVs.findMany();
+          },
+          TV(_, args) {
+            return prisma.tVs.findUnique({
+              where: {
+                id: args.id,
+              },
+            });
+          },
         },
       },
       plugins: [useCookies()],
